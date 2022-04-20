483b9139802911c98295760ee29f3e50918e0051
Tony Mascate <tonymascatedev@gmail.com>
2022-04-20 14:35:38 +0200

    git pull

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Crew from "./pages/Crew";
 import Destinations from "./pages/Destinations";
 import Home from "./pages/Home";
 
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' exact element={<Home />}></Route>
         <Route path='/destinations' element={<Destinations />}></Route>
+        <Route path='/crew' element={<Crew />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/pages/Crew.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Crew() {
+  return (
+    <section id="crew">
+      <div className="container">
+        <h2>
+          <span>02 </span>Notre équipe
+        </h2>
+        <div className="content">
+          <div className="text">
+            <h2>COMMANDER</h2>
+            <h1>DOUGLAS HURLEY</h1>
+            <p className="description">Douglas Gerald Hurley est un ingénieur américain, ancien pilote du Corps des Marines et ancien astronaute de la NASA. Il s'est lancé dans l'espace pour la troisième fois en tant que commandant de Crew Dragon Demo-2.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Crew;
